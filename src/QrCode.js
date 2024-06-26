03268e33db4c1d84ac91bf2bb7428fc97d9fcc14
ifeanyi305 <otijoseph305@gmail.com>
2024-06-26 12:10:32 +0100

    Remove unused codes

--- src/QrCode.js
+++ src/QrCode.js
@@ -5,23 +5,6 @@ const QrCode = () => {
   const [qrValue, setQrValue] = useState('https://example.com');
 
   return (
-    // <div>
-    //   <header className="App-header">
-    //     <h1>QR Code Generator</h1>
-    //     <QRCode
-    //       size={256}
-    //       bgColor={"#000000"}
-    //       fgColor={"#249F78"}
-    //       includeMargin={true}
-    //       value={qrValue}
-    //     />
-    //     <input
-    //       type="text"
-    //       value={qrValue}
-    //       onChange={(e) => setQrValue(e.target.value)}
-    //     />
-    //   </header>
-    // </div>
     <div className="min-h-screen bg-gray-100 flex items-center justify-center p-5">
       <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
         <header className="text-center mb-8">
